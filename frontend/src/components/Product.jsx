160655dc841a2b731d0300d73146cee78db8e0f5
juvin2899 <68566718+juvin2899@users.noreply.github.com>
2021-01-02 04:05:10 +0530

    create Rating.jsx and use in Product.jsx

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating.jsx";
 
 const Product = ({ product }) => {
-  return <Card className="my-3 p-3 rounded">
-  {product._id}
-  <a href={"/product/"+product._id}>
-  <Card.Img src={product.image} variant='top' />
-  </a>
+  return (
+    <Card className="my-3 p-3 rounded">
+      {product._id}
+      <a href={"/product/" + product._id}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
 
-  <Card.Body>
-  <a href={"/product/"+product._id}>
-  <Card.Title as="div">
-      <strong>{product.name}</strong>
-  </Card.Title>
-  </a>
-  <Card.Text as='div'>
-<div className="my-3">
-    {product.rating} from {product.numReviews} reviews
-</div>
-  </Card.Text>
-  <Card.Text as="h3">${product.price}</Card.Text>
-  </Card.Body>
-  </Card>;
+      <Card.Body>
+        <a href={"/product/" + product._id}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={product.numReviews + "reviews"}
+            
+          />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Product;
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-import products from "../products.js"
-import Product from "../components/Product.jsx"
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import products from "../products.js";
+import Product from "../components/Product.jsx";
 
 const HomeScreen = () => {
-    return (
-        <>
-        
-            <h1> Latest Products</h1>
-            <Row>
-                {products.map(product=>
-                {return <Col sm={12} md={6} lg={3}>
-                       <Product product={product} />
-                    </Col>
-                })}
-            </Row>
-        </>
-    )
-}
+  return (
+    <>
+      <h1> Latest Products</h1>
+      <Row>
+        {products.map((product) => {
+          return (
+            <Col key={product._id} sm={12} md={6} lg={3}>
+              <Product product={product} />
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
